4e11d6a211253419117d0d4dfcc42ba31185d800
Felix Martinez Casadiego <felix.martinezcasadiego@gmail.com>
2022-01-22 19:39:46 -0300

    Cuarta entrega

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import React, { Component } from 'react';
 import './App.css';
 
 // Carpeta Components
 import NavBar from './Components/NavBar';
+import ItemListContainer from './Components/ItemListContainer';
 
-function App() {
-  return (
-    <div className="App">
-
-      <NavBar />
-      <h1>Its Ramen !</h1> 
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div className='App'>
+        <NavBar />
+        <ItemListContainer 
+        title="Tipo de Ramen 1" 
+        description="Breve descipción 1"/>
+        <ItemListContainer 
+        title="Tipo de Ramen 2" 
+        description="Breve descipción 2"/>
+        
+      </div>
+    ); 
+  }
 }
 
 export default App;
--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -1,5 +1,18 @@
-const ItemListContainer = () => {
-    return
+import React from "react";
+
+const ItemListContainer = (props) => {
+    return(
+        <div>
+            
+            <h1>
+                {props.title}
+            </h1>
+            <h2>
+                {props.description}
+            </h2>
+            
+        </div>
+    )
 }
 
 export default ItemListContainer;
